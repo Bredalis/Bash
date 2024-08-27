7d941c220aa6c33642b2dae6bd8486b806ab33c7
PRO X <bredalisgautreaux@gmail>
2024-08-27 07:20:36 +0200

    feat: ✨ Se le agregó salto de línea

--- Arreglos.sh
+++ Arreglos.sh
@@ -4,17 +4,17 @@ numeros=(1 3 5 87)
 nombres=(Luisa Jeny Miguel Luis "Rosa Peralta")
 rangos=({A..Z} {1..20})
 
-echo "--- Valores de los arreglos ---"
+echo -e "\n--- Valores de los arreglos ---"
 echo "Arreglo numerico: ${numeros[*]}" 
 echo "Arreglo de nombres: ${nombres[*]}"
 echo "Arreglo de rangos: ${rangos[*]}"
 
-echo "--- Longitud de los arreglos ---"
+echo -e "\n--- Longitud de los arreglos ---"
 echo "Arreglo numerico: ${#numeros[*]}"
 echo "Arreglo de nombres: ${#nombres[*]}"
 echo "Arreglo de rangos: ${#rangos[*]}"
 
-echo "--- Posicion de los arreglos ---"
+echo -e "\n--- Posicion de los arreglos ---"
 echo "Posicion 0 arreglo numerico: ${numeros[0]}"
 echo "Posicion 1 Arreglo de nombres: ${nombres[1]}"
 echo "Posicion 2 Arreglo de rangos: ${rangos[2]}"
